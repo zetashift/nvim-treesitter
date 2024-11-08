819fe97118103a1cb4283961193cf53161ac6845
zetashift <rskaraya@gmail.com>
2024-11-08 20:53:14 +0100

    feat(unison): add indent queries

---
+++ queries/unison/indents.scm
@@ -0,0 +1,8 @@
+[(kw_then) (cases) (kw_equals) (kw_else) (do)] @indent.begin
+
+(literal_list) @indent.begin
+(tuple_or_parenthesized "(") @indent.begin
+
+(record) @indent.begin
+(record "}" @indent.branch @indent.end)
+[(literal_text)] @indent.auto
